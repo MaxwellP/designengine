502cae919de24950f1be971eb62d76969d2d05cb
strouticus <ajstrout@wpi.edu>
2014-11-25 04:02:42 -0500

    Initial GUI version
    Zones and cards are drawn on the screen. Zones (aside from player hands)
    are drawn as a grid in the center of the screen. No user interaction
    implemented yet.

--- Project/gui.js
+++ Project/gui.js
@@ -4,6 +4,7 @@
 Things to add to game description JSON:
 
  - Zone type: deck, showEachCard
+ - showZoneName bool?
 */
 
 var CARD_WIDTH = 70;
@@ -13,6 +14,8 @@ var CARD_DEFAULT_FONT_SIZE = 20;
 
 var ZONE_OUTLINE_WEIGHT = "3";
 var ZONE_MARGIN = 10;
+
+var SHOW_ZONE_NAMES = true;
 
 var canvas = document.getElementById('Canvas2D');
 var ctx = canvas.getContext('2d');
@@ -39,8 +42,10 @@ function renderFrame () {
 	clearFrame();
 
 	//TESTING
-	drawCard(50, 50, {value: "Hello"});
-	drawZoneBox(40, 40, CARD_WIDTH + 20, CARD_HEIGHT + 20);
+	//drawCard(50, 50, {value: "Hello"});
+	//drawZoneBox(40, 40, CARD_WIDTH + 20, CARD_HEIGHT + 20, "Hello Zone");
+	zoneGridLayout();
+	drawPlayersHands();
 }
 
 function clearFrame () {
@@ -50,7 +55,7 @@ function clearFrame () {
 
 // Drawing functions
 
-function drawCard (x, y, card) {
+function drawCard (card, x, y) {
 	ctx.save();
 	ctx.beginPath();
 	ctx.lineWidth = CARD_OUTLINE_WEIGHT;
@@ -80,38 +85,138 @@ function drawZoneBox (x, y, width, height, name) {
 	ctx.stroke();
 	ctx.restore();
 
-	if (name)
+	if (SHOW_ZONE_NAMES)
 	{
 		ctx.save();
 		ctx.font = "" + CARD_DEFAULT_FONT_SIZE + "px Georgia";
 		ctx.fillStyle = "#999999";
 		ctx.textAlign = "left";
-		ctx.fillText(name, x, y);
+		ctx.fillText(name, x, y - ZONE_OUTLINE_WEIGHT, width);
 		ctx.restore();
 	}
 }
 
 // ADD TYPE TO ZONE DEFINITION
-function drawZone (zone, type, x, y) {
+function drawZone (zone, x, y) {
 	var zoneWidth;
 	var zoneHeight;
 
-	//if (zone.type == "deck")
-	if (type == "deck")
+	// PLACEHOLDER
+	zone.type = "showEachCard"
+
+	if (zone.type == "deck")
 	{
 		zoneWidth = CARD_WIDTH + (ZONE_MARGIN * 2);
-		zoneHeight = CARD_HEIGHT + (ZONE_MARGIN * 2);
+		zoneHeight = getZoneHeight(zone);
 	}
-	//else if (zone.type == "showEachCard")
-	else if (type == "showEachCard")
+	else if (zone.type == "showEachCard")
 	{
-		var numCards = zone.cards[0].length;
-		zoneWidth = (numCards * CARD_WIDTH) + ((numCards + 1) * ZONE_MARGIN);
-		zoneHeight = CARD_HEIGHT + (ZONE_MARGIN * 2);
+		if (zone.cards[0])
+		{
+			var numCards = zone.cards.length;
+		}
+		else
+		{
+			var numCards = 1;
+		}
+		zoneWidth = getZoneWidth(zone);
+		zoneHeight = getZoneHeight(zone);
+
+		currentX = x + ZONE_MARGIN;
+		currentY = y + ZONE_MARGIN;
+		for (var i = 0; i < zone.cards.length; i++) {
+			drawCard(zone.cards[i], currentX, currentY);
+			currentX += CARD_WIDTH;
+			currentX += ZONE_MARGIN;
+		};
 	}
 
-	drawZoneBox(x, y, zoneWidth, zoneHeight);
+	drawZoneBox(x, y, zoneWidth, zoneHeight, zone.name);
 
+}
+
+function getZoneWidth (zone) {
+	var numCards = 1;
+	if (zone.cards[0] != undefined)
+	{
+		numCards = zone.cards.length;
+	}
+	return (numCards * CARD_WIDTH) + ((numCards + 1) * ZONE_MARGIN);
+}
+
+function getZoneHeight (zone) {
+	var zoneHeight = CARD_HEIGHT + (ZONE_MARGIN * 2);
+	return zoneHeight;
+}
+
+function zoneGridLayout () {
+	var centerX = canvas.width / 2;
+	var centerY = canvas.height / 2;
+
+	var numRows = Math.ceil(zones.length / ZONES_PER_LINE);
+
+	var grid = [[]];
+	var currentRow = 0;
+	var currentColumn = 0;
+
+	for (var i = 0; i < zones.length; i++) {
+		grid[currentRow][currentColumn] = zones[i];
+
+		if (i % ZONES_PER_LINE == ZONES_PER_LINE - 1)
+		{
+			currentRow++;
+			currentColumn = 0;
+			grid[currentRow] = [];
+		}
+		else
+		{
+			currentColumn++;
+		}
+	};
+
+	var rowHeight = getZoneHeight(grid[0][0]);
+	if (SHOW_ZONE_NAMES)
+	{
+		rowHeight += CARD_DEFAULT_FONT_SIZE;
+	}
+	
+	var gridHeight = (numRows * rowHeight) + ((numRows - 1) * ZONE_MARGIN);
+
+	var rowLengths = [];
+
+	for (var i = 0; i < numRows; i++) {
+		//var rowLength = ZONE_MARGIN;
+		var rowLength = 0;
+		for (var j = 0; j < grid[i].length; j++) {
+			rowLength += getZoneWidth(grid[i][j]);
+			rowLength += ZONE_MARGIN;
+		};
+		rowLengths[i] = rowLength;
+	};
+
+	var currentX = centerX - (rowLengths[0] / 2);
+	var currentY = centerY - (gridHeight / 2);
+	// Draw rows
+	for (var i = 0; i < numRows; i++) {
+		for (var j = 0; j < grid[i].length; j++) {
+			drawZone(grid[i][j], currentX, currentY);
+			currentX += getZoneWidth(grid[i][j]);
+			currentX += ZONE_MARGIN;
+		};
+		currentY += rowHeight;
+		currentY += ZONE_MARGIN;
+		currentX = centerX - (rowLengths[0] / 2);
+	};
+}
+
+function drawPlayersHands () {
+	drawZone(players[0], (canvas.width / 2) - (getZoneWidth(players[0]) / 2), (canvas.height) - (getZoneHeight(players[0])));
+	var player2Height = 0;
+	if (SHOW_ZONE_NAMES)
+	{
+		player2Height += CARD_DEFAULT_FONT_SIZE;
+	}
+	drawZone(players[1], (canvas.width / 2) - (getZoneWidth(players[1]) / 2), player2Height);
 }
 
 function MousePos (e) {
