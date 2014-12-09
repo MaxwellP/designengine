2d3c5e4cb5afa0be41d7cfbf8e36947dda2cde9e
Maxwell Perlman <mhperlman@wpi.edu>
2014-12-09 00:55:58 -0500

    Updates to showoff
    Have fun with moving things around

--- Project/showoff.js
+++ Project/showoff.js
@@ -8,24 +8,18 @@ THE FOLLOWING GOES IN THE WIN CONDITION FUNCTION
 var gamestate = arguments[arguments.length - 1];
 */
 
-
 function increaseValueResult()
 {
 	var player = arguments[arguments.length - 3];
 	var move = arguments[arguments.length - 2];
 	var gamestate = arguments[arguments.length - 1];
-	console.log("Increasing the value");
-	console.log(move.card.value);
 	move.card.value += 1;
-	console.log(move.card.value);
 };
-
 function increaseValueCheckLegality()
 {
 	var player = arguments[arguments.length - 3];
 	var move = arguments[arguments.length - 2];
 	var gamestate = arguments[arguments.length - 1];
-	console.log("You can increase the value");
 	return true;
 };
 
@@ -34,21 +28,157 @@ function decreaseValueResult()
 	var player = arguments[arguments.length - 3];
 	var move = arguments[arguments.length - 2];
 	var gamestate = arguments[arguments.length - 1];
-	var zone = arguments[0];
-	console.log("Decreasing the value");
-	console.log(move.card.value);
 	move.card.value -= 1;
-	console.log(move.card.value);
 };
-
 function decreaseValueCheckLegality()
 {
 	var player = arguments[arguments.length - 3];
 	var move = arguments[arguments.length - 2];
 	var gamestate = arguments[arguments.length - 1];
-	var zone = arguments[0];
-	console.log("You can decrease the value");
 	return true;
+};
+
+function toAnyZoneResult()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+	zone.cards.push(move.card);
+	player.cards.splice(player.cards.indexOf(move.card),1);
+};
+function toAnyZoneCheckLegality()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+
+	for(var i = 0; i < player.cards.length; i += 1)
+	{
+		if(player.cards[i] == move.card)
+		{
+			return true;
+		}
+	}
+	return false;
+};
+
+function toPublic1Result()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+	zone.cards.push(move.card);
+	player.cards.splice(player.cards.indexOf(move.card),1);
+};
+function toPublic1CheckLegality()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+
+	for(var i = 0; i < player.cards.length; i += 1)
+	{
+		if(player.cards[i] == move.card && zone.name == "public1")
+		{
+			return true;
+		}
+	}
+	return false;
+};
+
+function toPublic2Result()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+	zone.cards.push(move.card);
+	player.cards.splice(player.cards.indexOf(move.card),1);
+};
+function toPublic2CheckLegality()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+	for(var i = 0; i < player.cards.length; i += 1)
+	{
+		if(player.cards[i] == move.card && zone.name == "public2")
+		{
+			return true;
+		}
+	}
+	return false;
+};
+
+function toPublic3Result()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+
+	zone.cards.push(move.card);
+	player.cards.splice(player.cards.indexOf(move.card),1);
+};
+function toPublic3CheckLegality()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone = arguments[0];
+	for(var i = 0; i < player.cards.length; i += 1)
+	{
+		if(player.cards[i] == move.card && zone.name == "public3")
+		{
+			return true;
+		}
+	}
+	return false;
+};
+
+
+function swapZonesResult()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+
+	var zone1 = arguments[0];
+	var zone2 = arguments[1];
+	var zone1Size = zone1.cards.length;
+	var zone2Size = zone2.cards.length;
+
+	for(var i = 0; i < zone1Size; i += 1)
+	{
+		zone2.cards.push(zone1.cards[0]);
+		zone1.cards.shift();
+	}
+
+	for(var i = 0; i < zone2Size; i += 1)
+	{
+		zone1.cards.push(zone2.cards[0]);
+		zone2.cards.shift();
+	}
+};
+function swapZonesCheckLegality()
+{
+	var player = arguments[arguments.length - 3];
+	var move = arguments[arguments.length - 2];
+	var gamestate = arguments[arguments.length - 1];
+	var zone1 = arguments[0];
+	var zone2 = arguments[1];
+
+	if(zone1.name != zone2.name)
+	{
+		return true;
+	}
+
+	return false;
 };
 
 function showoffWinCondition()
